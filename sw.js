101ef8ff993acebbd7bf8152681f0bbbe99f721a
H07vk <Holger.utzt@gmx.de>
2026-09-23 16:34:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // Update zuverlässig, verwirft den alten Cache (siehe "activate" unten) und lädt
 // die neuen Dateien frisch nach.
 
-const APP_VERSION = "1.2.0";
+const APP_VERSION = "1.3.0";
 const CACHE_NAME = "foto-ocr-shell-" + APP_VERSION;
 const SHELL_FILES = [
   "./",
